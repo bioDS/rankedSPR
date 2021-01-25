295e2155d469600a818c2ca23b9ad33a716384af
lenacoll <lena.collienne@gmail.com>
2021-01-25 14:32:46 +1300

    add simulation scripts

---
+++ simulate_coalescent/src/simulate_coalescent.R
@@ -0,0 +1,19 @@
+# Author: Lena Collienne
+# simulate coalescent trees
+
+library(ape)
+library(rstudioapi)
+
+# Setting the working directory -- might need to be changes when not using RStudio
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd("../simulated_trees")
+
+coalescent_tree_sim <- function(num_leaves, num_trees) {
+  trees = c()
+  for (i in 1:num_trees){
+    trees[[i]] <- rcoal(num_leaves)
+  }
+  write.nexus(trees, file = paste("coal_trees_", num_leaves, "_n_", num_trees, "_N.nex", sep = ""))
+}
+
+coalescent_tree_sim(50,20000)
