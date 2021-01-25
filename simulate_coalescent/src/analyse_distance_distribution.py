5a1edceba933020ee96bc56a8f5a59df8091dd25
lenacoll <lena.collienne@gmail.com>
2021-01-25 15:24:48 +1300

    read tree_file in main

--- simulate_coalescent/src/analyse_distance_distribution.py
+++ simulate_coalescent/src/analyse_distance_distribution.py
@@ -12,10 +12,8 @@ from dct_parser.tree_io import *
 import rf_distance_analysis as rf
 
 
-def rnni_distances_tree_pairs(filename):
+def rnni_distances_tree_pairs(tree_list):
     # returns array of distances between every pair of trees i,i+1 (even i) in given file and number of leaves
-    print("Read trees")
-    tree_list = read_nexus(filename, ranked = True)[0]
     distances = []
     progress = 0.05 #for printing progress
     print("Computing RNNI distances")
@@ -28,10 +26,8 @@ def rnni_distances_tree_pairs(filename):
         # print(i, tree_to_cluster_string(tree_list.trees[i]), i+1, tree_to_cluster_string(tree_list.trees[i+1]))
     return(distances, int(tree_list.trees[0].num_leaves))
 
-def rnni_distance_focal(filename, index):
+def rnni_distance_focal(tree_list, index):
     # returns array of distances from chosen focal tree (tree number i in nexus file)
-    print("Read trees")
-    tree_list = read_nexus(filename, ranked = True)[0]
     distances = []
     progress = 0.05 #for printing progress
     print("Computing RNNI distances")
@@ -46,21 +42,28 @@ def rnni_distance_focal(filename, index):
 
 if __name__ == '__main__':
     # Get input trees:
-    tree_file = input("What is the file with trees?\n")
+    filename = input("What is the file with trees?\n")
+    print("Read trees")
+    tree_list = read_nexus(filename, ranked = True)[0]
+    print("Done reading trees")
+    num_trees = tree_list.num_trees
+    num_leaves = tree_list.trees[0].num_leaves
+    rnni_diameter = int((num_leaves-1)*(num_leaves-2)/2)
+
     # # Plotting RNNI distances
-    # distances_rnni,num_leaves = rnni_distances_tree_pairs(tree_file)
-    # rnni_diameter = int((num_leaves-1)*(num_leaves-2)/2)
+    # distances_rnni,num_leaves = rnni_distances_tree_pairs(tree_list)
     # plt.hist(distances_rnni, bins = rnni_diameter, range = (0, rnni_diameter))
     # plt.show()
-    # # Plot RNNI distances to random focal tree
+
+    # # Plotting RNNI distances to random focal tree
     # num_trees = 2*len(distances_rnni)
-    num_trees = 20000
     index = np.random.randint(0,num_trees)
-    focal_dist = rnni_distance_focal(tree_file, index)
-    plt.hist(focal_dist, bins = rnni_diameter, range = (0, rnni_diameter))
+    focal_dist = rnni_distance_focal(tree_list, index)
+    plt.hist(focal_dist, range = (0, rnni_diameter))
     plt.show()
+
     # # Plotting RF distances:
-    # distances_rf, num_leaves = rf.rf_distances_tree_pairs(tree_file)
+    # distances_rf, num_leaves = rf.rf_distances_tree_pairs(filename)
     # rf_diameter = int(2*(num_leaves - 1))
     # plt.hist(distances_rf, bins = rf_diameter, range = (0, rf_diameter))
     # plt.show()
