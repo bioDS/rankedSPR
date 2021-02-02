192042dc714871a795c0526d0095c718aee7413c
lenacoll <lena.collienne@gmail.com>
2021-02-02 15:55:06 +1300

    fixing plot saving: in plot.py functions

--- xplore/src/plots.py
+++ xplore/src/plots.py
@@ -6,17 +6,19 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-def plot_hist(d, filehandle, bins):
+def plot_hist(d, bins, filehandle = ''):
     # Shows and saves histogram (to specified file)
     df = pd.DataFrame(data = d)
     sns.histplot(data=df, bins = bins, stat = 'density', legend = False)
-    plt.savefig(filehandle)
+    if filehandle != '':
+        plt.savefig(filehandle)
     plt.tight_layout()
     plt.show()
 
-def plot_dots(d, filehandle):
+def plot_dots(d, filehandle = ''):
     # Shows and saves values (to specified file)
     plt.plot(d, linestyle = 'None', marker = 'o', markersize = 6)
-    plt.savefig(filehandle)
+    if filehandle != '':
+        plt.savefig(filehandle)
     plt.tight_layout()
     plt.show()
