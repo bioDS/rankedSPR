a3ab92c2fd3c0cbbc4e7ff59d9500896587e9955
lenacoll <lena@lenacoll.de>
2021-11-04 12:13:52 +1300

    thin out log file to analyse sample with tracer

--- xplore/src/thin_out_sample.py
+++ xplore/src/thin_out_sample.py
@@ -1,6 +1,8 @@
 # Take a nexus file containing trees and create a new file with every mth tree of the input file -- for thinning out long tree files from BEAST run where every tree is saved in file
 __author__ = 'Lena Collienne'
 
+import re
+
 
 def thin_out_sample(input_file, output_file, m):
     # Read sample from input_file and write into output_file every mth tree
@@ -22,5 +24,25 @@ def thin_out_sample(input_file, output_file, m):
     o_file.close()
 
 
+def thin_out_log(input_file, output_file, m):
+    # Read sample from input_file and write into output_file every mth tree
+    count = 0
+    o_file = open(output_file, "w")
+    with open(input_file) as i_file:
+        for line in i_file:
+            if count == 0:
+                # Copy header + first tree from input to output file
+                o_file.write(line)
+                if re.match("0", line):
+                    # First line containing information for a sample -- update counter
+                    count += 1
+            else:
+                count +=1
+                if (count-1) % m == 0:
+                    o_file.write(line)
+    o_file.close()
+
+
 if __name__ == '__main__':
-    thin_out_sample("../simulations/posterior/primates/primates.trees", "../simulations/posterior/primates/primates_small.trees", 10000)
+    # thin_out_sample("../simulations/posterior/primates/primates.trees", "../simulations/posterior/primates/primates_small.trees", 10000)
+    thin_out_log("../simulations/posterior/primates/primate-mtDNA.log", "../simulations/posterior/primates/primate-mtDNA_small.log", 10000)
