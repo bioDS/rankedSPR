2c898615a9ee3d4b4c8bce9b8b99ac1e51fc7f51
lenacoll <lena@lenacoll.de>
2022-05-17 15:22:01 +1200

    testing

--- test.py
+++ test.py
@@ -20,19 +20,36 @@ s = "(((3:1,4:1):1,2:2):1,1:3);"
 q1 = "((((1:1,2:1):1,4:2):1,5:3):1,3:4);"
 q2 = "((((4:1,5:1):1,3:2):1,2:3):1,1:4);"
 
-# ct = read_newick(q1, factor = 0)
-# cr = read_newick(q2, factor = 0)
+a = "(((2:1,4:1):2,5:3):1,(1:2,3:2):2);"
+b = "(((1:1,3:1):2,2:3):1,(4:2,5:2):2);"
+
+ct = read_newick(a, factor = 0)
+cr = read_newick(b, factor = 0)
 
 # print('symmetric cluster difference:', symmetric_cluster_diff(ct,cr))
 
-# path = rankedspr_path_mrca_cluster_diff(ct, cr)
-# print('distance: ', path.num_trees-1)
+# tree_list = sim_coal(5,100)
+# for i in range(0, tree_list.num_trees - 2, 2):
+#     print(i)
+#     if len(rankedspr_bfs(tree_list.trees[i], tree_list.trees[i+1], 0))-1 != rankedspr_path_mrca_cluster_diff(tree_list.trees[i], tree_list.trees[i+1], 0).num_trees-1:
+#         print("Not equal distance:")
+#         print(tree_to_cluster_string(tree_list.trees[i]))
+#         print(tree_to_cluster_string(tree_list.trees[i+1]))
+
+# for tree in rankedspr_bfs(ct, cr, hspr=0):
+#     print(tree)
+# print('distance: ', len(rankedspr_bfs(ct, cr, 0))-1)
+
+# path = rankedspr_path_mrca_cluster_diff(ct, cr, 0)
+# print('approximated distance: ', path.num_trees-1)
 # for i in range(0,path.num_trees):
 #     print(tree_to_cluster_string(path.trees[i]))
 #     print(symmetric_cluster_diff(path.trees[i],cr) + mrca_differences(path.trees[i],cr))
 
 # rankedspr_path_restricting_neighbourhood(ct,cr)
 
+rankedspr_seidel(6)
+orbit_size_identity_caterpillar(6)
 
 # a = "((((3:1,4:1):1,5:2):2,6:4):1,(1:3,2:3):2);"
 # b = "((1:1,2:1):4,(((4:2,5:2):1,3:3):1,6:4):1);"
@@ -40,19 +57,32 @@ q2 = "((((4:1,5:1):1,3:2):1,2:3):1,1:4);"
 # c = "((1:1,2:1):5,((((3:2,4:2):1,5:3):1,6:4):1,7:5):1)"
 # d = "((1:1,2:1):5,((3:2,7:2):3,((4:3,5:3):1,6:4):1):1);"
 
-# tree1 = read_newick(q1, factor=0)
-# tree2 = read_newick(q2, factor=0)
+t1 = "(((2:1,3:1):2,(1:2,4:2):1):1,5:4);"
+t2 = "((((1:1,5:1):1,4:2):1,3:3):1,2:4);"
 
-# print(rankedspr_path_top_down_symm_diff(tree1, tree2))
-# print(symm_cluster_diff(tree1, tree2, 2*tree1.num_leaves-3))
+t3 = "(((1:1,5:1):2,(3:2,4:2):1):1,2:4);"
+t4 = "((((1:1,5:1):1,4:2):1,2:3):1,3:4);"
 
+t5 = "(((2:1,5:1):1,3:2):2,(1:3,4:3):1);"
+t6 = "(((1:1,5:1):2,3:3):1,(2:2,4:2):2);"
+
+
+# tree1 = read_newick(t5, factor=0)
+# tree2 = read_newick(t6, factor=0)
+
+# print("Top-down approach:")
+# print("approx distance:", rankedspr_path_top_down_symm_diff(tree1, tree2))
+
+# print("Actual shortest path:")
 # path = rankedspr_bfs(tree1, tree2)
 # print('distance:', len(path)-1)
 # print('path:')
 # for tree in path:
 #     print(tree)
 
+# print(symm_cluster_diff(tree1, tree2, 2*tree1.num_leaves-3))
 
+# test_top_down_neighbourhood_search(5,100)
 
 # n=7
 # m=1000
@@ -95,8 +125,8 @@ q2 = "((((4:1,5:1):1,3:2):1,2:3):1,1:4);"
 # coal_pw_spr_dist(5, 10000, hspr = 1)
 
 
-# test_restricted_neighbourhood_search(5,10000)
-test_top_down_neighbourhood_search(5,10)
+# test_restricted_neighbourhood_search(5,1000, hspr = 1)
+# test_top_down_neighbourhood_search(5,10)
 
 # print(sum(rankedSPR_adjacency(4, hspr=0)[0]))
 # print(sum(rankedSPR_adjacency(4)[0]))
