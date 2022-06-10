ee0ced34656b92f36f4e195565c2bf9278550848
lenacoll <lena@lenacoll.de>
2022-06-10 15:07:16 +1200

    check if bottom-up idea works when only consider ctrees -- it doesn't

--- rankedSPR.py
+++ rankedSPR.py
@@ -123,6 +123,31 @@ def test_restricted_neighbourhood_search(num_leaves, num_tree_pairs, hspr = 1):
     print('correct distance:', correct_distance, 'out of', num_tree_pairs)
 
 
+def test_restricted_neighbourhood_search_caterpillar(num_leaves, num_trees, hspr = 1):
+    # Compute adjacency matrix & distance matrix
+    rspr_adj = rankedSPR_adjacency(num_leaves, hspr)
+    rspr_distances = np.ascontiguousarray(rspr_adj[0], dtype=np.int32)
+    _seidel.seidel(rspr_distances, rspr_distances.shape[0])
+
+    # Now simulate trees to be used to check distance computation
+    t_list = sim_cat(num_leaves, num_trees)
+    ctree = identity_caterpillar(num_leaves)
+    correct_distance = 0
+    ctree_index = rspr_adj[1][tree_to_cluster_string(ctree)]
+    for i in range(0,num_trees):
+        tree_index = rspr_adj[1][tree_to_cluster_string(t_list.trees[i])]
+        # print(tree_to_cluster_string(t_list.trees[i]))
+        # print(tree_to_cluster_string(t_list.trees[i+1]))
+        # print(rspr_distances[tree_index][tree2_index], rankedspr_path_restricting_neighbourhood(t_list.trees[i],t_list.trees[i+1]))
+        if (rspr_distances[tree_index][ctree_index] == rankedspr_path_restricting_neighbourhood(t_list.trees[i], ctree, hspr)):
+            correct_distance += 1
+        else:
+            print("tree1:", tree_to_cluster_string(t_list.trees[i]))
+            print("tree2:", tree_to_cluster_string(ctree))
+            print("correct distance:", rspr_distances[tree_index][ctree_index], "approximated distance:", rankedspr_path_restricting_neighbourhood(t_list.trees[i],ctree,hspr))
+    print('correct distance:', correct_distance, 'out of', num_trees)
+
+
 def test_top_down_neighbourhood_search(num_leaves, num_tree_pairs):
     # Compute adjacency matrix & distance matrix
     rspr_adj = rankedSPR_adjacency(num_leaves)
