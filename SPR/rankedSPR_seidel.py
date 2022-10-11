ea9c706519d56773dbb2d9849f525ae81cbed97a
Lena Collienne <lena@lenacoll.de>
2022-10-11 17:42:18 +1300

    mv all distance stuff to rankedSPR_seidel, only keeping adjacency computation + BFS in rankedspr_adjacency (formally rankedspr_distances)

--- SPR/rankedSPR_seidel.py
+++ SPR/rankedSPR_seidel.py
@@ -1,6 +1,6 @@
 from os.path import exists
 from unlabelled_rankedspr_distances import *
-from rankedspr_distances import *
+from SPR.rankedspr_adjacency import *
 import time
 from numpy.ctypeslib import ndpointer
 import numpy as np
@@ -68,3 +68,53 @@ def unlabelled_ranked_spr_seidel(n, hspr=True):
     time2 = time.time()
     print("C Seidel took {:.3f}ms".format((time2 - time1) * 1000.0))
     print("diameter: ", np.amax(A))
+
+def read_distance_matrix(num_leaves, hspr=False, unlabelled=1):
+    # read distance matrix and corresponding trees and return them as matrix and two dicts (index to tree and tree to index)
+    # Read distance matrix
+    if unlabelled != 0:
+        if hspr == False:
+            d = np.load('output/distance_matrix_' + str(num_leaves) +
+                        '_leaves.npy')
+            f = open('output/tree_dict_' + str(num_leaves) + '_leaves.txt',
+                     'r')
+        elif hspr == True:
+            d = np.load('output/distance_matrix_' + str(num_leaves) +
+                        '_leaves_hspr.npy')
+            f = open('output/tree_dict_' + str(num_leaves) +
+                     '_leaves_hspr.txt', 'r')
+    else:
+        if hspr == False:
+            d = np.load('output/unlabelled_distance_matrix_' +
+                        str(num_leaves) + '_leaves.npy')
+            f = open('output/unlabelled_tree_dict_' +
+                     str(num_leaves) + '_leaves.txt', 'r')
+        elif hspr == True:
+            d = np.load('output/unlabelled_distance_matrix_' +
+                        str(num_leaves) + '_leaves_hspr.npy')
+            f = open('output/unlabelled_tree_dict_' + str(num_leaves) +
+                     '_leaves_hspr.txt', 'r')
+
+    # Put all trees into a dict (note that indices are sorted increasingly in file)
+    tree_strings = f.readlines()
+    index = 0
+    tree_dict = dict()
+    tree_index_dict = dict()
+    for tree_str in tree_strings:
+        tree_str = tree_str.split(" ")[1].split("\n")[0]
+        tree_dict[tree_str] = index
+        tree_index_dict[index] = tree_str
+        index += 1
+    return (d, tree_dict, tree_index_dict)
+
+
+def get_distance_matrix(num_leaves, hspr):
+    '''get the distance matrix for trees on num_leaves leaves for hspr (HSPR=TRUE) or rspr.'''
+    if (hspr == True and not exists("output/distance_matrix_" +
+                                    str(num_leaves) + "_leaves_hspr.npy")
+        ) or (hspr == False and not exists("output/distance_matrix_" +
+                                           str(num_leaves) + "_leaves.npy")):
+        print("start computing distance matrix")
+        rankedspr_seidel(num_leaves, hspr)
+        print("finish computing distance matrix")
+    return read_distance_matrix(num_leaves, hspr)
