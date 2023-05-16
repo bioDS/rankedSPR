e21b896af61559326c1852c50a786ad36f6b79d2
lenacoll <lena@lenacoll.de>
2023-05-16 16:38:56 -0700

    make test output for cluster theorem counterexample easier to read

--- test_for_paper.py
+++ test_for_paper.py
@@ -21,7 +21,8 @@ def check_approx_alg(num_leaves):
             approx_path = rankedspr_path_bottom_up_hspr(tree1, tree2)
             approx_dist = approx_path.num_trees - 1
             if approx_dist != d[i][j]:
-                print("For the trees", tree_index_dict[i], "and", tree_index_dict[j], "the approximated HSPR distance is", approx_dist)
+                print("For the trees", tree_index_dict[i], "and", tree_index_dict[j],
+                    "the approximated HSPR distance is", approx_dist)
                 print("The exact distance is", d[i][j])
                 return(False)
     return(True)
@@ -31,9 +32,34 @@ def main():
     get_diameter(n)
     check_approx_alg(n)
 
-    tree1 = read_from_cluster("[{4,5}:1,{1,2}:2,{1,2,3}:3,{1,2,3,4,5}:4]")
-    tree2 = read_from_cluster("[{1,2}:1,{4,5}:2,{1,2,3}:3,{1,2,3,4,5}:4]")
-    all_shortest_paths(tree1, tree2)
+    tree1_str = "[{4,5}:1,{1,2}:2,{1,2,3}:3,{1,2,3,4,5}:4]"
+    tree2_str = "[{1,2}:1,{4,5}:2,{1,2,3}:3,{1,2,3,4,5}:4]"
+    tree1 = read_from_cluster(tree1_str)
+    tree2 = read_from_cluster(tree2_str)
+
+    # dictionary containing predecessors of tree2 for all paths from tree1 to tree2
+    pred_dict = all_shortest_paths(tree1, tree2)
+
+    # check if any tree in the predecessor dictionary (other than tree1 and tree2)
+    # contains the cluster {1,2,3}
+    cluster_found = False
+    for tree_str in pred_dict:
+        if tree_str != tree1_str and tree_str != tree2_str and "{1,2,3}" in tree_str:
+            print(tree_str + " contains the cluster {1,2,3}")
+            cluster_found = True
+        for pred_str in pred_dict[tree_str]:
+            if pred_str != tree1_str and pred_str != tree2_str and "{1,2,3}" in pred_str:
+                print(tree_str + " contains the cluster {1,2,3}")
+                cluster_found = True
+        if cluster_found:
+            break
+    if cluster_found:
+        print("The shared clusters {1,2,3} is present in a tree on a shortes path between "
+        + tree1_str + " and " + tree2_str)
+    else:
+        print("The shared clusters {1,2,3} is not present in a tree on a shortes path between "
+        + tree1_str + " and " + tree2_str)
+
 
 if __name__ == "__main__":
     main()
